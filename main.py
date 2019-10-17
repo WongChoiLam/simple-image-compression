b28e2f84cbb41130af99d08fc8f79ddd11ca3551
Wong Choi Lam <julian.wcl@outlook.com>
2019-10-17 22:12:12 +0100

    restructured code

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def distances_from_centres(X, centres):
             flag = False
         else:
             ret = np.concatenate((ret,distances), axis=0)
-    ret = ret.reshape((16, M*N)).T
+    ret = ret.reshape((K, M*N)).T
     return ret
 def get_index(X, centres):
     (M,N,C) = np.shape(X)
@@ -31,40 +31,54 @@ def get_index(X, centres):
     distances = distances_from_centres(X, centres)
     min_index = np.argmin(distances, axis = 1)
     return min_index
-
-random.seed()
-K = 16  #K
-max_iteration = 10  #maximum number of iterations
-
-img = pyplot.imread('in\lenna.jpg')
-(M,N,C) = np.shape(img)
-
-num_pixel = M * N
-
-#initialize 16 cluster centre
-sample_index = np.array(random.sample(range(M*N), K))
-cluster_centres = img.reshape(M*N, 3)[sample_index]
-#initialize index array
-min_index = None
-for iteration in range(max_iteration):
-    #compute the average of each group of samples
-    min_index = get_index(img, cluster_centres)
-    total = np.zeros((16,3))
-    total_samples = np.zeros(16)
+def new_cluster_centres(X, min_index, K):
+    (M, N, C) = np.shape(X)
+    total = np.zeros((K, C))
+    total_samples = np.zeros(K)
     for i in range(M*N):
-        total[min_index[i]] += img.reshape((M*N, C))[i]
+        total[min_index[i]] += X.reshape((M*N, C))[i]
         total_samples[min_index[i]] += 1
     total_samples = np.atleast_2d(total_samples).T
-    total_samples = np.tile(total_samples, (1,3))
+    total_samples = np.tile(total_samples, (1,C))
     avg_distance = total / total_samples
-    #update the cluster centres with the average value of assigned samples
-    cluster_centres = avg_distance
-cluster_centres = np.around(cluster_centres, decimals = 0)
-compressed_img = np.zeros((M*N,C))
-for i in range(M*N):
-    compressed_img[i] = cluster_centres[min_index[i]]
-compressed_img = compressed_img.reshape((M,N,C))
-compressed_img = np.uint8(compressed_img)
-print(compressed_img)
-imgplot = pyplot.imshow(compressed_img)
+    return avg_distance
+def rebuild_image(size, min_index, cluster_centres):
+    (M,N,C) = size
+    new_image = np.zeros((M*N,C))
+    cc = np.around(cluster_centres)
+    for i in range(M*N):
+        new_image[i] = cc[min_index[i]]
+    new_image = new_image.reshape((M,N,C))
+    return new_image
+
+def compress_img(img, K, max_iteration):
+    random.seed()
+    (M,N,C) = np.shape(img)
+    num_pixel = M * N
+
+    #initialize cluster centre
+    sample_index = np.array(random.sample(range(M*N), K))
+    cluster_centres = img.reshape(M*N, 3)[sample_index]
+    #initialize index array
+    min_index = None
+    for iteration in range(max_iteration):
+        print(str(iteration + 1) + "\t/" + str(max_iteration) + '\n')
+        #compute the average of each group of samples
+        min_index = get_index(img, cluster_centres)
+        #update the cluster centres with the average value of assigned samples
+        cluster_centres = new_cluster_centres(img, min_index, K)
+    new_img = rebuild_image((M,N,C), min_index, cluster_centres)
+    return new_img.astype(np.int32)
+
+img = pyplot.imread('in\lenna.jpg')
+compressed_img16 = compress_img(img, 16, 10)
+fig = pyplot.figure()
+a = fig.add_subplot(1, 2, 1)
+imgplot = pyplot.imshow(img)
+a.set_title("Original")
+
+b = fig.add_subplot(1, 2, 2)
+imgplot = pyplot.imshow(compressed_img16)
+b.set_title("K = 16")
+
 pyplot.show()
